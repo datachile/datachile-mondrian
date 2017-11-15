941ceae0579720d3fa4340282f5310eff776575a
Manuel Aristaran <jazzido@jazzido.com>
2017-11-15 13:44:06 -0300

    merge_translations.rb: skip rows that don't match

--- scripts/merge_translations.rb
+++ scripts/merge_translations.rb
@@ -8,7 +8,10 @@ schema = File.open(schema_path) { |f| Nokogiri::XML(f) }
 CSV.parse(STDIN, headers: true) do |row|
   it = schema.xpath("//InlineTable[@alias='#{row['table_name']}']")
   it_row = it.at_xpath("./Rows/Row/Value[@column='#{row['id_column_name']}' and text()='#{row['id_value']}']/parent::Row")
-
+  if it_row.nil?
+    STDERR.puts "Can't find <Row> for table #{row['table_name']} and id #{row['id_value']}"
+    next
+  end
   it_row.at_xpath(".//Value[@column='description']").content = row['description'].strip
   it_row.at_xpath(".//Value[@column='es_description']").content = row['es_description'].strip
 end
